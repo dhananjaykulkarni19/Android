32c90f1fa5976efcaf7f270f54716d024f39697c
dhananjaykulkarni19 <dhananjaygithub@gmail.com>
2015-01-31 11:40:04 +0530

    Create VolleySingleton.java

---
+++ VolleySingleton.java
@@ -0,0 +1,55 @@
+package com.example
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import android.app.Application;
+
+
+public class VolleySingleton extends Application{
+
+	public static final String TAG = "VolleyTag";
+	private RequestQueue  mRequestQueue;
+	private static VolleySingleton mRequestInstance;
+	
+	
+	@Override
+	public void onCreate() {
+	
+		super.onCreate();
+		mRequestInstance = this;
+		
+	}
+	
+	public static synchronized VolleySingleton getInstance(){
+		return mRequestInstance;
+	}
+	
+	public RequestQueue getRequestQueue(){
+		
+		if (mRequestQueue == null){
+			mRequestInstance.mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+		}
+			return mRequestInstance.mRequestQueue;
+	}
+	
+	public <T> void addToRequestQueue(Request<T> req){
+		
+		req.setTag(TAG);
+		getRequestQueue().add(req);
+	}
+	
+	public void cancelPendingRequests(Object tag){
+		
+		if(mRequestQueue!=null){
+			mRequestQueue.cancelAll(tag);
+		}
+	}	
+}
+public <T> void addToRequestQueue(Request<T> req, String tag) {
+    
+    req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+
+    VolleyLog.d("Adding request to queue: %s", req.getUrl());
+
+    getRequestQueue().add(req);
+}
